22544103a62ecb2f47545b99fad00aae72bc7c40
0xjaqbek <jaqbek.eth@gmail.com>
2025-09-25 10:24:28 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/lecture-transcription-pwa/sw.js', { scope: '/lecture-transcription-pwa/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/student/sw.js', { scope: '/student/' })})}
